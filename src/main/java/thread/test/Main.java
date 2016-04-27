f3f7fe27c5764f5e6d1cfde56a7760594f866a6b
Laszlo Kishalmi <laszlo_kishalmi@epam.com>
2016-04-28 01:32:36 +0200

    Added cancellationTokenSource. The issue is still not reproducible.

--- src/main/java/thread/test/Main.java
+++ src/main/java/thread/test/Main.java
@@ -3,6 +3,7 @@ package thread.test;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.gradle.tooling.CancellationTokenSource;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 
@@ -12,18 +13,22 @@ import org.gradle.tooling.ProjectConnection;
  */
 public class Main implements Runnable {
 
-    private Object timer = new Object();
+    private final Object TIMER = new Object();
     
     @Override
     public void run() {
         GradleConnector gconn = GradleConnector.newConnector().useGradleVersion("2.13");
         ThreadGroup tg = Thread.currentThread().getThreadGroup();
+        CancellationTokenSource cancellationTokenSource = GradleConnector.newCancellationTokenSource();
         ProjectConnection pconn = null;
         try {
             System.out.println("Active Threads [pre conn]: " + tg.activeCount());
             pconn = gconn.forProjectDirectory(new File(System.getProperty("user.dir"), "../services-clubs-service")).connect();
             System.out.println("Active Threads [pre build]: " + tg.activeCount());
-            pconn.newBuild().withArguments("clean", "build", "-x", "check").setStandardOutput(System.out).run();
+            pconn.newBuild().withArguments("clean", "build", "-x", "check")
+                    .withCancellationToken(cancellationTokenSource.token())
+                    .setStandardOutput(System.out)
+                    .run();
             System.out.println("Active Threads [post build]: " + tg.activeCount());
         } finally {
             if (pconn != null) {
@@ -36,15 +41,16 @@ public class Main implements Runnable {
                 System.out.println("Active Threads [post close]: " + tg.activeCount());
             }
         }
-        synchronized(timer) {
+        // A very raw mimic what NetBeans do for waiting for all the created threads end in the TG
+        synchronized(TIMER) {
             try {
                 while (tg.activeCount() > 1) {
-                    timer.wait(1000);
+                    TIMER.wait(1000);
                 }
             } catch (InterruptedException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             } finally {
-                timer.notifyAll();
+                TIMER.notifyAll();
                 System.out.println("Active Threads [final]: " + tg.activeCount());
             }
         }
